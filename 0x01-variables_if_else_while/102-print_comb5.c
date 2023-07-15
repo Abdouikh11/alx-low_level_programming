2c89fc3f38dc3c52da50573d068f4a7213861922
Abderrahim Ouikhelfen <abderrahim.ouikhelfen@gmail.com>
2023-07-15 16:48:04 -0700

    12.Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
-
 /**
-  * main - Entry point
-  * Return: Always 0 (Success)
-  */
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	
+	int i, j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i; j <= 99; j++)
+		{
+			int num1 = i / 10;
+			int num2 = i % 10;
+			int num3 = j / 10;
+			int num4 = j % 10;
+
+			putchar('0' + num1);
+			putchar('0' + num2);
+			putchar(' ');
+			putchar('0' + num3);
+			putchar('0' + num4);
+
+			if (i != 99 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
 }
