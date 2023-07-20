0f21157e46f1428d0bb7e0d609cd4d079dd57b9d
Abderrahim Ouikhelfen <abderrahim.ouikhelfen@gmail.com>
2023-07-20 14:50:25 -0700

    Fucntion that prints numbers from 0 to 9 only using putchar

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Function that prints number from 0 to 9 only using putchar
+ * Returns: Alwyas 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	int n;
+	
+	for (n = 0; n < 10; n++)
+	{
+		_putchar(n + 48);
+	}
+	_putchar('\n');
+}
