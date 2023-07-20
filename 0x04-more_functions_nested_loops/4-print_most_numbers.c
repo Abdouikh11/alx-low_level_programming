98b24f79290b588fd5a8dd6bd5f22fd61746cdc6
Abderrahim Ouikhelfen <abderrahim.ouikhelfen@gmail.com>
2023-07-20 15:26:24 -0700

    function that prints numbers from 0 to 9 without 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,20 @@
 #include "main.h"
 
 /**
- * main - 
- *
+ * print_most_numbers - Function that prints numbers from 0 to 9 without 2 & 4
+ * Return: Always 0 (Success)
  */
 
 void print_most_numbers(void)
 {
-	
+	int i;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		else
+			_putchar(i + 48);
+	}
+	_putchar('\n');
 }
