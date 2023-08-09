7693d5b72defe1c8ed9ed5863f1432057888ca0c
Abderrahim Ouikhelfen <abderrahim.ouikhelfen@gmail.com>
2023-08-09 14:52:10 -0700

    Function that splits a string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,76 +1,37 @@
 #include "main.h"
-
-/**
- * count_words - Count the number of words in a string
- * @str: The string to count words in
- * Return: returns the number of words in the string
- */
-int count_words(char *str)
-{
-	int i, count = 0, in_word = 0;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] != ' ' && !in_word)
-		{
-			count++;
-			in_word = 1;
-		}
-		else if (str[i] == ' ')
-			in_word = 0;
-	}
-
-	return (count);
-}
-
 /**
  * strtow - Split a string into words
  * @str: The string to split
- *
  * Return: A pointer to an array of strings (words)
  */
+
 char **strtow(char *str)
 {
-	int i, j, k, l, num_words, word_len;
+	int i, j, l, num_words = 0, word_len;
 	char **words;
 
-	if (str == NULL || str[0] == '\0')
+	if (!str || !*str)
 		return (NULL);
-
-	num_words = count_words(str);
-	if (num_words == 0)
-		return (NULL);
-
+	for (i = 0; str[i]; i++)
+		if ((i == 0 || str[i - 1] == ' ') && str[i] != ' ')
+			num_words++;
 	words = malloc(sizeof(char *) * (num_words + 1));
-	if (words == NULL)
+	if (!words)
 		return (NULL);
-
 	for (i = 0, j = 0; i < num_words; i++)
 	{
 		while (str[j] == ' ')
 			j++;
-
-		word_len = 0;
-		while (str[j + word_len] != ' ' && str[j + word_len] != '\0')
-			word_len++;
-
-		words[i] = malloc(sizeof(char) * (word_len + 1));
-		if (words[i] == NULL)
-		{
-			for (k = 0; k < i; k++)
-				free(words[k]);
-			free(words);
+		for (word_len = 0; str[j + word_len] && str[j + word_len] != ' '; word_len++)
+			;
+		words[i] = malloc(word_len + 1);
+		if (!words[i])
 			return (NULL);
-		}
-
 		for (l = 0; l < word_len; l++)
 			words[i][l] = str[j + l];
 		words[i][l] = '\0';
-
 		j += word_len;
 	}
-
 	words[i] = NULL;
-
 	return (words);
 }
