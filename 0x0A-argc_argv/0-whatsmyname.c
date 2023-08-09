b42b34b44dd248ebac49beb6676df1aa42e45068
Abderrahim Ouikhelfen <abderrahim.ouikhelfen@gmail.com>
2023-08-09 12:25:45 -0700

    Function that prints it's name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,14 +9,7 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-
 	(void)argc;
-
-	for (i = 0; argv[0][i] != '\0'; i++)
-	{
-		_putchar(argv[0][i]);
-	}
-	_putchar('\n');
+	printf("%s\n",argv[0]);
 	return (0);
 }
