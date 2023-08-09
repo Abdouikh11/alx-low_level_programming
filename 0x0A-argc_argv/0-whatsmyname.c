255f646b712236fd49ba17c8794c709c2ef9541e
Abderrahim Ouikhelfen <abderrahim.ouikhelfen@gmail.com>
2023-08-09 14:16:09 -0700

    program that prints its name, followed by a new lin

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,14 +2,15 @@
 
 /**
  * main - Entry point
- * @argc: the number of arguments
- * @argv: the array of strings as pointer
- * Return: return 0 alwyas success
- ***/
+ * @argc: number of arguments
+ * @argv: the array of strings
+ * Return: returns 0
+ *
+ */
 
 int main(int argc, char *argv[])
 {
 	(void)argc;
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
