8334fd9c14153b3acb44b9cd06d5805dc0d8772e
Abderrahim Ouikhelfen <abderrahim.ouikhelfen@gmail.com>
2023-08-09 12:23:09 -0700

    Function that prints it;s name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,7 +4,7 @@
  * main - Entry point
  * @argc: the number of arguments
  * @argv: the array of strings as pointer
- *
+ * Return: return 0 alwyas success
  ***/
 
 int main(int argc, char *argv[])
