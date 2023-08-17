413ffa83866ec0254a3b909ab3d783d76549a679
Abderrahim Ouikhelfen <abderrahim.ouikhelfen@gmail.com>
2023-08-16 19:57:36 -0700

    done

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two integers
+ * @a: the first
+ * @b: the second
+ * Return: returns the results
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substrcts two integers
+ * @a: the first
+ * @b: the second
+ * Return: returns the results
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two integers
+ * @a: the first
+ * @b: the second
+ * Return: returns the results
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two integers
+ * @a: the first
+ * @b: the second
+ * Return: returns the results
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo of two integers
+ * @a: the first
+ * @b: the second
+ * Return: returns the results
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
+
