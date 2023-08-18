4b2517a8331efc8c79fd51127bcf6ebae3bfe73c
Abderrahim Ouikhelfen <abderrahim.ouikhelfen@gmail.com>
2023-08-17 19:12:41 -0700

    a variadic function that prints alll numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function that prints numbers
+ * @separator: the sperator tha separates two numbers
+ * @n: numbers of paramters 
+ *
+ *
+ *
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list arg;
+	unsigned int i;
+
+	va_start(arg, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(arg, int));
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(arg);
+}
