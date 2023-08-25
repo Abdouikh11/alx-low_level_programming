2daa4b9e9bd3408c8e0bde069aadec835c3d8973
Abdouikh11 <abderrahim.ouikhelfen@gmail.com>
2023-08-24 19:56:23 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,13 @@
 #include "lists.h"
 
 /**
- *  __attribute__ - prints s sentence
- *
+ * __attribute__ - prints s sentence
+ * premain - function that persms
  *
  */
 
 void __attribute__ ((constructor)) premain()
 {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
