d219f318ba7919e77d11af436935daa7a697558a
Abdouikh11 <abderrahim.ouikhelfen@gmail.com>
2023-08-24 20:11:38 -0700

    ok

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,22 +1,22 @@
 section .data
-hello db "Hello, Holberton",0
-format db "%s",10,0
+    hello db "Hello, Holberton",0
+    format db "%s",10,0
 
 section .text
-global main
-extern printf
+    global main
+    extern printf
 
 main:
-; Push format string onto the stack
-push format
-; Push hello string onto the stack
-push hello
-; Call printf
-call printf
-; Clean up the stack
-add rsp, 16
+    ; Push format string onto the stack
+    push format
+    ; Push hello string onto the stack
+    push hello
+    ; Call printf
+    call printf
+    ; Clean up the stack
+    add rsp, 16
 
-; Exit the program
-mov rax, 60         ; syscall: exit
-xor rdi, rdi        ; status: 0
-syscall
+    ; Exit the program
+    mov rax, 60         ; syscall: exit
+    xor rdi, rdi        ; status: 0
+    syscall
