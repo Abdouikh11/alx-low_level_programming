6b750b5c51ca30a0fc8eb206e08e74821daa36ae
Abderrahim Ouikhelfen <abderrahim.ouikhelfen@gmail.com>
2023-11-02 11:40:33 -0700

    done

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,10 +4,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-<<<<<<< HEAD
 
-=======
->>>>>>> 56eb6e20781235ab776e4e91d54fba8e31ce5999
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
