cd0dd59ce98cee26459b07ed0834f13cbcf3fc11
Abderrahim Ouikhelfen <abderrahim.ouikhelfen@gmail.com>
2023-11-02 11:59:51 -0700

    done for task advnaced 4

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -297,16 +297,16 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 		exit(98);
 	}
-	check_elf(header->ident);
+	check_elf(header->e_ident);
 	printf("ELF Header:\n");
-	print_magic(header->ident);
-	print_class(header->ident);
-	print_data(header->ident);
-	print_version(header->ident);
-	print_osabi(header->ident);
-	print_abi(header->ident);
-	print_type(header->e_type, header->ident);
-	print_entry(header->e_entry, header->ident);
+	print_magic(header->e_ident);
+	print_class(header->e_ident);
+	print_data(header->e_ident);
+	print_version(header->e_ident);
+	print_osabi(header->e_ident);
+	print_abi(header->e_ident);
+	print_type(header->e_type, header->e_ident);
+	print_entry(header->e_entry, header->e_ident);
 	free(header);
 	close_elf(fd);
 	return (0);
